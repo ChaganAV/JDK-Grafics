39989b95b96e2e79d652af1ad6db39e469cc7a87
Alexandr <jwchaganav@gmail.com>
2023-09-26 10:19:06 +0400

    начал логику игры

--- src/main/java/Map.java
+++ src/main/java/Map.java
@@ -1,15 +1,37 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Random;
 
 public class Map extends JPanel {
     private int panelWidth;
     private int panelHeight;
     private int cellHeight;
     private int cellWidth;
+    private static final Random RANDOM = null;
+    private final int HUMAN_DOT = 1;
+    private final int AI_DOT = 2;
+    private final int EMPTY_DOT = 0;
+    private int fieldSizeX = 3;
+    private int fieldSizeY = 3;
+    private char[] field;
     Map() {
-        //setBackground(Color.BLACK);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                update(e);
+            }
+        });
     }
-
+    private void update(MouseEvent e){
+        int cellX = e.getX() / cellWidth;
+        int cellY = e.getY() / cellHeight;
+        System.out.printf("getX=%d, getY=%d, cellWidth=%d, cellHeight=%d, x=%d, y=%d\n",
+                e.getX(), e.getY(), cellWidth, cellHeight, cellX,cellY);
+        repaint();
+    }
     void startNewGame(int mode, int fSzX, int fSzY, int wLen){
         System.out.printf("Mode: %d;\nSize: x=%d, y=%d;\nWin Length: %d",
                 mode,fSzX,fSzY,wLen);
@@ -21,6 +43,7 @@ public class Map extends JPanel {
         super.paintComponent(g);
         render(g);
     }
+
 
     private void render(Graphics g){
         panelWidth = getWidth();
